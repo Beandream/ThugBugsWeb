b47e91fe190387243bb4f92f793d7c8dd282503f
Beandream <beandreamed@gmail.com>
2024-09-08 17:32:22 -0400

    add movable synced sprites

--- public/game.js
+++ public/game.js
@@ -16,8 +16,71 @@ var config = {
     update: update
   }
 };
-
 var game = new Phaser.Game(config);
-function preload() { }
-function create() { }
-function update() { }
+
+function preload() {
+  this.load.image('bug', 'assets/duck.png');
+  this.load.image('otherPlayer', 'assets/chick.png');
+}
+
+function create() {
+  var self = this;
+  this.socket = io();
+  this.otherPlayers = this.physics.add.group();
+
+  this.socket.on('currentPlayers', (players) => {
+    Object.keys(players).forEach((id) => {
+      if (players[id].playerId === self.socket.id) {
+        addPlayer(self, players[id]);
+      } else {
+        addOtherPlayers(self, players[id]);
+      }
+    })
+  });
+
+  this.socket.on('newPlayer', (playerInfo) => {
+    addOtherPlayers(self, playerInfo);
+  });
+
+  this.socket.on('disconnectPlayer', (playerId) => {
+    self.otherPlayers.getChildren().forEach((otherPlayer) => {
+      if (playerId === otherPlayer.playerId) {
+        otherPlayer.destroy();
+      }
+    })
+  })
+
+  this.socket.on('playerUpdated', (playerInfo) => {
+    self.otherPlayers.getChildren().forEach((otherPlayer) => {
+      if (playerInfo.playerId === otherPlayer.playerId) {
+        otherPlayer.setPosition(playerInfo.x, playerInfo.y);
+      }
+    })
+  })
+
+  // this.input.on('pointerdown', (e) => {
+  //   this.player.x = e.x;
+  //   this.player.y = e.y;
+  // })
+
+}
+
+function update() {
+  if (!this.player) return
+
+  this.socket.emit('playerUpdate', {x: this.player.x, y: this.player.y});
+  if (this.input.activePointer.isDown) {
+    this.player.x = this.input.activePointer.x;
+    this.player.y = this.input.activePointer.y;
+  }
+}
+
+function addPlayer(self, playerInfo) {
+  self.player = self.physics.add.image(playerInfo.x, playerInfo.y, "bug").setOrigin(0.5, 0.5);
+}
+
+function addOtherPlayers(self, playerInfo) {
+  const otherPlayer = self.add.sprite(playerInfo.x, playerInfo.y, 'otherPlayer').setOrigin(0.5, 0.5);
+  otherPlayer.playerId = playerInfo.playerId;
+  self.otherPlayers.add(otherPlayer);
+}
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const port = process.env.PORT || 8080;
 
+var players = {};
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
@@ -12,11 +14,32 @@ app.get('/', function (req, res) {
 
 io.on('connection', (socket) => {
     console.log('user connected');
+
+    players[socket.id] = {
+        x: Math.floor(Math.random() * 700) + 50,
+        y: Math.floor(Math.random() * 500) + 50, 
+        playerId: socket.id
+    }
+
+    socket.emit('currentPlayers', players);
+
+    socket.broadcast.emit('newPlayer', players[socket.id]);
+
     socket.on('disconnect', function () {
         console.log('user disconnected');
+
+        delete players[socket.id];
+
+        io.emit('disconnectPlayer', socket.id);
+    });
+
+    socket.on('playerUpdate', (position) => {
+        players[socket.id].x = position.x;
+        players[socket.id].y = position.y;
+        socket.broadcast.emit('playerUpdated', players[socket.id]);
     });
 })
 
 server.listen(port, function () {
-    console.log(`Listening on port ${port}`);
+    console.log(`Listening on port ${port}`, `http://localhost:${port}`);
 });
